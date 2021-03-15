72487ae81c8c53c9f2105773b26ed59c42a5d957
fedoroff94 <artsemfedziankou@gmail.com>
2021-03-16 01:06:08 +0300

    init: start reverse-int-task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if(n < 0){
+        n = -n;
+    }
+    let string = n.toString();
+    let arr = [];
+    for(char of string){
+        arr.push(char);
+    }
+    return +arr.reverse().join('');
+};
+
+
